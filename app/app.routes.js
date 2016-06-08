7681adc4f91c1b39fcf61d65db4a19f4078ad5b5
hair4 <hair4@llnl.gov>
2016-06-08 10:13:22 -0700

    Edited Readme.md

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,20 +1,19 @@
 angular.module('myApp.routes', [])
-    .config(function ($stateProvider) {
-
-        $stateProvider
-            .state('root', {
-                url: '/',
-                template: '<root-component></root-component>',
-                controller: 'rootController'
-            })
-            .state('root.views', {
-                url: 'views',
-                template: '<views></views>',
-                controller: 'viewsController'
-            })
-            .state('root.views.details', {
-                url: '/:id',
-                template: '<view-details></view-details>',
-                controller: 'viewDetailsController'
-            })
-    });
+.config(function ($stateProvider) {
+    $stateProvider
+    .state('root', {
+        url: '/',
+        template: '<root-component></root-component>',
+        controller: 'rootController'
+    })
+    .state('root.views', {
+        url: 'views',
+        template: '<views></views>',
+        controller: 'viewsController'
+    })
+    .state('root.views.details', {
+        url: '/:id',
+        template: '<view-details></view-details>',
+        controller: 'viewDetailsController'
+    })
+});
